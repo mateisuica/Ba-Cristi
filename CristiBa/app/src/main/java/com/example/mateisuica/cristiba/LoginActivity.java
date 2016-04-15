af7f33494665a225c36ac0cbf53ee75216c4aa00
QuadroQ <matei_suica@yahoo.com>
2016-04-15 10:33:55 +0300

    Switched Login with register

--- CristiBa/app/src/main/java/com/example/mateisuica/cristiba/LoginActivity.java
+++ CristiBa/app/src/main/java/com/example/mateisuica/cristiba/LoginActivity.java
@@ -1,13 +1,43 @@
 package com.example.mateisuica.cristiba;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_acitivty);
+        setContentView(R.layout.activity_login);
+
+        final EditText username = (EditText)findViewById(R.id.username);
+        final EditText password = (EditText)findViewById(R.id.password);
+        Button login = (Button)findViewById(R.id.loginButton);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String usernameString = username.getText().toString();
+                String passwordString = password.getText().toString();
+
+                boolean usernameAndPasswordWereValidated = isValid(usernameString, passwordString);
+
+                if(usernameAndPasswordWereValidated == true) {
+                    Intent intent = new Intent(LoginActivity.this, ContactsList.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(LoginActivity.this, "Wrong username and/or password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private boolean isValid(String user, String pass) {
+        return user.equals(pass);
     }
 }
