c812ab12f7fdc7617aa23e9e0c17aeb138933402
Matei Suica <matei.suica@gmail.com>
2016-05-12 16:50:06 +0300

    Done app

---
+++ CristiBa/app/src/main/java/com/example/mateisuica/cristiba/BaCristiReceiver.java
@@ -0,0 +1,22 @@
+package com.example.mateisuica.cristiba;
+
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+
+import com.backendless.push.BackendlessBroadcastReceiver;
+
+/**
+ * Created by Matei on 5/12/2016.
+ */
+public class BaCristiReceiver extends BackendlessBroadcastReceiver {
+
+    @Override
+    public boolean onMessage(Context context, Intent intent) {
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(context, R.raw.ba_cristi);
+        mediaPlayer.start();
+
+        return true;
+    }
+}
--- CristiBa/app/src/main/java/com/example/mateisuica/cristiba/ContactsList.java
+++ CristiBa/app/src/main/java/com/example/mateisuica/cristiba/ContactsList.java
@@ -17,6 +17,10 @@ import com.backendless.BackendlessCollection;
 import com.backendless.BackendlessUser;
 import com.backendless.async.callback.AsyncCallback;
 import com.backendless.exceptions.BackendlessFault;
+import com.backendless.messaging.DeliveryOptions;
+import com.backendless.messaging.MessageStatus;
+import com.backendless.messaging.PublishOptions;
+import com.backendless.messaging.PushBroadcastMask;
 import com.backendless.persistence.BackendlessDataQuery;
 
 import java.util.ArrayList;
@@ -24,6 +28,7 @@ import java.util.List;
 
 public class ContactsList extends AppCompatActivity {
 
+    private List<Contact> mList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,10 +39,11 @@ public class ContactsList extends AppCompatActivity {
 
         String whereClause = "firstPerson = '" + currentUser.getEmail() + "'";
         BackendlessDataQuery dataQuery = new BackendlessDataQuery();
-        dataQuery.setWhereClause( whereClause );
-        Backendless.Persistence.of( Contact.class ).find(dataQuery, new AsyncCallback<BackendlessCollection<Contact>>() {
+        dataQuery.setWhereClause(whereClause);
+        Backendless.Persistence.of(Contact.class).find(dataQuery, new AsyncCallback<BackendlessCollection<Contact>>() {
             @Override
             public void handleResponse(BackendlessCollection<Contact> response) {
+                mList = response.getData();
                 contacts.setAdapter(new ContactAdapter(ContactsList.this, R.layout.contact_row, response.getData()));
             }
 
@@ -48,14 +54,35 @@ public class ContactsList extends AppCompatActivity {
         });
 
 
-
-
-
         contacts.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                MediaPlayer mediaPlayer = MediaPlayer.create(ContactsList.this, R.raw.ba_cristi);
-                mediaPlayer.start();
+                String whereClause = "email = '" + mList.get(position).getSecondPerson() + "'";
+                BackendlessDataQuery dataQuery = new BackendlessDataQuery();
+                dataQuery.setWhereClause(whereClause);
+                Backendless.Persistence.of(BackendlessUser.class).find(dataQuery, new AsyncCallback<BackendlessCollection<BackendlessUser>>() {
+                    @Override
+                    public void handleResponse(BackendlessCollection<BackendlessUser> response) {
+
+                        BackendlessUser user = response.getData().get(0);
+                        DeliveryOptions deliveryOptions = new DeliveryOptions();
+
+                        deliveryOptions.addPushSinglecast( (String)user.getProperty("gcmToken") );
+
+                        PublishOptions publishOptions = new PublishOptions();
+                        publishOptions.putHeader( "android-ticker-text", "You just got a push notification!" );
+                        publishOptions.putHeader( "android-content-title", "This is a notification title" );
+                        publishOptions.putHeader( "android-content-text", "Push Notifications are cool" );
+
+                        MessageStatus status = Backendless.Messaging.publish( "Hi Devices!", publishOptions, deliveryOptions );
+
+                    }
+
+                    @Override
+                    public void handleFault(BackendlessFault fault) {
+
+                    }
+                });
             }
         });
     }
